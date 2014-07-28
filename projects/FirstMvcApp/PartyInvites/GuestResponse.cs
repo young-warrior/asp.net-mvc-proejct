fed80b19c890c3e534aa00d2c910f09f5abfeaeb
PjatlinDima <dima.part@gmail.com>
2014-07-29 00:42:45 +0300

    * FirstProject * Add form

---
+++ projects/FirstMvcApp/PartyInvites/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites
+{
+    //Доменный класс GuestResponse
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+
+    }
+}
